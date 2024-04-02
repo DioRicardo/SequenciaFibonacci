490837cef138ca89c2fb6eb641551aed453a7e72
Dio Ricardo <dio.vieira@live.com>
2024-04-02 14:30:50 -0300

    Subindo o Projeto

---
+++ SequenciaFibonacci/Modelos/Fibonacci.cs
@@ -0,0 +1,32 @@
+﻿namespace SequenciaFibonacci.Modelos;
+
+internal class Fibonacci
+{
+    private int PreviousNumber { get; set; } = 0;
+    private int Number { get; set; } = 1;
+
+    private int NextNumber { get; set; } = 0;
+
+    public string SequenciaFibonacci(int inputNumber)
+    {
+        if (inputNumber == 0)
+        {
+            return $"O número {inputNumber} percente à sequencia de Fibonacci.";
+        }
+        while (Number < inputNumber)
+        {
+            NextNumber = PreviousNumber + Number;
+            PreviousNumber = Number;
+            Number = NextNumber;
+        }
+
+        if (Number == inputNumber)
+        {
+            return $"O número {inputNumber} percente à sequencia de Fibonacci.";
+        }
+        else
+        {
+            return $"O número {inputNumber} não percente à sequencia de Fibonacci.";
+        }
+    }
+}
---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,10 @@
+﻿using SequenciaFibonacci.Modelos;
+
+Fibonacci resposta = new();
+
+Console.Write("Digite um número para saber se pertence à sequência de Fibonacci: ");
+int inputNumber = int.Parse(Console.ReadLine()!);
+
+string mensagem = resposta.SequenciaFibonacci(inputNumber);
+
+Console.WriteLine(mensagem);
